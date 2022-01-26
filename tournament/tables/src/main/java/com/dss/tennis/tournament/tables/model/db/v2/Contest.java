d830d845a4ebecbceff5b392fa88bfa9fae38a4d
tiver69 <kibko.sasha@gmail.com>
2022-01-26 20:50:08 +0200

    [24] Add db changes and initial hibernate integration for double contests

---
+++ tournament/tables/src/main/java/com/dss/tennis/tournament/tables/model/db/v2/Contest.java
@@ -0,0 +1,111 @@
+package com.dss.tennis.tournament.tables.model.db.v2;
+
+import com.dss.tennis.tournament.tables.model.db.v1.Score;
+import com.dss.tennis.tournament.tables.model.db.v1.Tournament;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+//@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Contest {
+    private int id;
+    private byte round;
+    private Score score;
+    private Tournament tournament;
+    private Byte winner;
+    private Date date;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "round", nullable = false)
+    public byte getRound() {
+        return round;
+    }
+
+    public void setRound(byte round) {
+        this.round = round;
+    }
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "score_id", nullable = false)
+    public Score getScore() {
+        return score;
+    }
+
+    public void setScore(Score score) {
+        this.score = score;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "tournament_id", nullable = false)
+    public Tournament getTournament() {
+        return tournament;
+    }
+
+    public void setTournament(Tournament tournament) {
+        this.tournament = tournament;
+    }
+
+    @Basic
+    @Column(name = "winner", nullable = true)
+    public Byte getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Byte winner) {
+        this.winner = winner;
+    }
+
+    @Basic
+    @Column(name = "date", nullable = true)
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contest contest = (Contest) o;
+
+        if (id != contest.id) return false;
+        if (round != contest.round) return false;
+        if (winner != null ? !winner.equals(contest.winner) : contest.winner != null) return false;
+        if (date != null ? !date.equals(contest.date) : contest.date != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (int) round;
+        result = 31 * result + (winner != null ? winner.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        return result;
+    }
+}
---
+++ tournament/tables/src/main/java/com/dss/tennis/tournament/tables/model/db/v2/DoubleContest.java
@@ -0,0 +1,42 @@
+package com.dss.tennis.tournament.tables.model.db.v2;
+
+import com.dss.tennis.tournament.tables.model.db.v1.Team;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+//@Entity
+@PrimaryKeyJoinColumn(name = "contest_id")
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoubleContest extends Contest {
+
+    private Team teamOne;
+    private Team teamTwo;
+
+    @ManyToOne
+    @JoinColumn(name = "team_one_id", nullable = false)
+    public Team getTeamOne() {
+        return teamOne;
+    }
+
+    public void setTeamOne(Team teamOne) {
+        this.teamOne = teamOne;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "team_two_id", nullable = false)
+    public Team getTeamTwo() {
+        return teamTwo;
+    }
+
+    public void setTeamTwo(Team teamTwo) {
+        this.teamTwo = teamTwo;
+    }
+}
