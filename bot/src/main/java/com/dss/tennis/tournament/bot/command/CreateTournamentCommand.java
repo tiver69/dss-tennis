55613b0f63a7ec8aa86be516cf62051f678d9741
tiver69 <kibko.sasha@gmail.com>
2022-12-21 00:21:44 +0200

    General project structure refactoring

--- bot/src/main/java/com/dss/tennis/tournament/bot/command/CreateTournamentCommand.java
+++ bot/src/main/java/com/dss/tennis/tournament/bot/command/CreateTournamentCommand.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
-import java.util.*;
-
 @Component
 public class CreateTournamentCommand implements DssBotCommand {
 
--- bot/src/main/java/com/dss/tennis/tournament/bot/helper/KeyboardMarkupHelper.java
+++ bot/src/main/java/com/dss/tennis/tournament/bot/helper/KeyboardMarkupHelper.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
-import java.util.*;
-
 @Component
 public class KeyboardMarkupHelper {
 
